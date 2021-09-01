5b0862db9088dd9cadb70716f40ee7d90ee1c7ee
John Butler <john.butler984@my.tccd.edu>
2021-09-01 02:33:56 -0500

    Initial version

---
+++ labs/Lab1/JohnButler.AdventureGame.ConsoleHost/Program.cs
@@ -0,0 +1,23 @@
+﻿/*=================
+John Butler
+ITSE 1430 Fall 2021
+Adventure Game
+=================*/
+
+using System;
+
+namespace JohnButler.AdventureGame.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintTitleScreen();
+        }
+        static void PrintTitleScreen ()
+        {
+            Console.WriteLine("ITSE 1430 Adventure Game");
+            Console.WriteLine("------------------------");
+        }
+    }
+}
