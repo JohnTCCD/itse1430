569b53a929e5cc00f8220610150201a7c8b9a379
John Butler <john.butler984@my.tccd.edu>
2021-09-04 15:57:07 -0500

    Added GetCommand() Added FindRoom() Added DisplayRoom8() Added do while loop in Main()

--- labs/Lab1/JohnButler.AdventureGame.ConsoleHost/Program.cs
+++ labs/Lab1/JohnButler.AdventureGame.ConsoleHost/Program.cs
@@ -12,12 +12,48 @@ namespace JohnButler.AdventureGame.ConsoleHost
     {
         static void Main(string[] args)
         {
+            int roomNumber = 8;
+            string input;
             PrintTitleScreen();
+            FindRoom(roomNumber);
+
+            do   //TODO: Impliment core loop for game.
+            {
+                input = GetCommand();
+            } while (input != "Quit");
+
+            Console.WriteLine("Ending game.");
         }
-        static void PrintTitleScreen ()
+        static void PrintTitleScreen()
         {
             Console.WriteLine("ITSE 1430 Adventure Game");
             Console.WriteLine("------------------------");
+            //TODO: Display discription/story for game.
+        }
+        static string GetCommand()
+        {
+            //TODO: Display help menu.
+            Console.WriteLine("What will you do?");
+            string command = Console.ReadLine();
+            return command;
+        }
+        static void FindRoom(int roomNumber)
+        {
+            switch(roomNumber)
+            {   //TODO: Add rest of the room numbers.
+                case 8:
+                DisplayRoom8();
+                break;
+
+                default:
+                Console.WriteLine("Invalid input.");
+                break;
+            }
+        }
+        static void DisplayRoom8()
+        {
+            //TODO: Display room info.
+            Console.WriteLine("Room 8");
         }
     }
 }
