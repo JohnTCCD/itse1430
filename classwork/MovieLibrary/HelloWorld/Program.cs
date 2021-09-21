33f3c09767e3012d3641117364859d589fb41ea1
John Butler <john.butler984@my.tccd.edu>
2021-09-20 21:37:12 -0500

    Added movie class

---
+++ classwork/MovieLibrary/HelloWorld/Program.cs
@@ -0,0 +1,222 @@
+﻿/*
+ * file header
+ * Your Name
+ * ITSE 1430 Fall 2021
+ */
+using System;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Demoing the logical operators
+            static void DemoLogicalOperators()
+            {
+                // Logical AND - true if both operands are true
+                // Logical OR - true if either operand is true
+                //   X   Y   &&    ||
+                //--------------------------
+                //   F   F    F    F
+                //   F   T    F    T
+                //   T   F    F    T
+                //   T   T    T    T
+
+                // NOT 
+                //   F   !F = T
+                //   T   !T = F
+            }
+
+            static void DemoRelationalOperators ()
+            {
+                int x = 10, y = 20;
+
+                bool isLessThan = x < y;
+                bool isLessThanOrEqual = x <= y;
+
+                bool isGreaterThan = x > y;
+                bool isGreaterThanOrEqual = x >= y;
+
+                bool isEqual = x == y;
+                bool isNotEqual = x != y;
+            }
+
+            static void DemoCombinationOperators ()
+            {
+                //Works for more than just arithmetic
+                int x = 10;
+
+                x += 10;  // x = x + 10;
+                x -= 20;  // x = x - 20;
+                x *= 3;   // x = x * 3;
+                x /= 5;   // x = x / 5;
+                x %= 2;   // x = x % 2;
+            }
+
+            static void DemoPrefixPostfixOperators()
+            {
+                int x = 10, y;
+
+                //Prefix increment
+                y = ++x; // x = 11, y = 11
+
+                //Prefix decrement
+                y = --x; // x = 10, y = 10
+
+                //Postfix increment
+                y = x++; // x = 11, y = 10
+
+                //Postfix decrement
+                y = x--; // x = 10, y = 11
+            }
+
+            static void DemoAssignmentOperator ()
+            {
+                int x;
+                int y;
+                int z;
+
+                x = 10;
+                y = 10;
+                z = 10;
+
+                //left associative (E1 op E2) => E1, E2, op
+                //right associative operators (E1 op E2) => E2, E1, op
+                x = y = z = 20; //x = (y = (z = 20))
+            }
+
+            static void DemoConditionalOperator ()
+            {
+                int grade = 70;
+
+                string passStatus;
+
+                if (grade < 60)
+                    passStatus = "Not Passing";
+                else
+                    passStatus = "Passing";
+
+                //Terninary - 3 operands
+                // E(bool) ? E(true) : E(false)
+                string passStatus2 = (grade < 60) ? "Not Passing" : "Passing";
+            }
+
+            static void DemoStrings()
+            {
+                string firstName = "Bob";
+
+                //Relationals work, case sensitive
+                bool isBob = firstName == "Bob";
+
+                //String literals
+                string literal1 = "Hello World";
+
+                //Escape sequence - \?, only work in string literals
+                // \n => newline
+                // \t => horizontal tab
+                // \\ => slash
+                // \" => double quote
+                // \' => character literal, single quote
+                literal1 = "Hello\nWorld";
+                string quoteString = "Hello \"Bob\"";
+
+                string filePath = "C:\\windows\\system32\\tasks";
+                string verbatimFilePath = @"C:\windows\system32\tasks";
+
+                //String length `Length` => how many characters
+                int length = literal1.Length;
+
+                //Empty string
+                string emptyString = "";  //.Length = 0
+                string emptyString2 = String.Empty;  //Length = 0
+                bool areEmptyStringsEqual = String.Empty == "";  //True
+
+                // null != empty string
+                // default value of strings is null
+                string nullString = null;
+                bool areEqualNull = "" == null;  // false
+
+                // string is the univerals type in C#
+                // all expressions are convertable to string using .ToString
+                string asString = length.ToString();  // length as a string
+                asString = 10.ToString();  // "10
+                asString = areEqualNull.ToString();  //"False"
+
+                // Comparison
+                string value1 = "Hello", value2 = "hello";
+                bool areEqual = value1 == value2;
+                bool compareCaseSensitive = String.Compare(value1, value2) == 0;
+                bool compareCaseInsensitive = String.Compare(value1, value2, true) == 0; // Perferred
+
+                compareCaseSensitive = value1.CompareTo(value2) == 0; //works but not safe
+
+                string lowerValue1 = value1.ToLower();
+                string upperValue1 = value1.ToUpper();
+                compareCaseInsensitive = value1.ToUpper() == value2.ToUpper();
+
+                // Concatenattion: first last year
+                int year = 2021;
+                firstName = "Bob";
+                string lastName = "Smith";
+                string name = firstName + " " + lastName + " " + year;  // Bob Smith
+                name = String.Concat(firstName, " ", lastName, " ", year);  // For larger number of strings
+
+                var builder = new StringBuilder();  // For complex situations
+                builder.Append(firstName);
+                builder.Append(" ");
+                builder.Append(lastName);
+                builder.Append(" ");
+                builder.Append(year);
+                name = builder.ToString();
+
+                name = String.Join(" ", firstName, lastName, year);
+
+                // Misc.
+                bool startsWithB = name.StartsWith("B");
+                startsWithB = name.StartsWith("B", StringComparison.CurrentCultureIgnoreCase);
+
+                bool endsWith9 = name.EndsWith("9");
+                endsWith9 = name.EndsWith("9", StringComparison.CurrentCultureIgnoreCase);
+
+                // Removes leading and trailing whitespace
+                string normalizedName = name.Trim();
+                //name.TrimStart().TrimEnd();
+
+                //Some useful functions
+                //name.Substring(character); //hets a subset of string
+                //name.IndexOf(character); //Finds a character
+
+                name = name.PadLeft(50); //Add enough spaces on left to make string length 50
+                name = name.PadRight(50); //Same, but on the right
+
+                // Empty string checking
+                bool isEmpty;
+                isEmpty = name == "";  //Not always going to work correctly for you
+                isEmpty = name.Length == 0;  // Will crash if null
+
+                // handle null
+                isEmpty = (name != null) ? name == "" : true;
+                isEmpty = name == null || name == "";
+                isEmpty = (name != null) ? name.Length == 0 : true;
+                isEmpty = String.IsNullOrEmpty(name); //Perferred
+
+                // Formatting - Hello first last, the year is year
+                name = "Hello " + firstName + " " + lastName + ", the year is " + year + ".";
+                name = String.Format("Hello {0} {1}, the year is {2}.", firstName, lastName, year);
+                Console.WriteLine("Hello {0} {1}, the year is {2}.", firstName, lastName, year);
+
+                decimal price = 8.75M;
+                string priceString = price.ToString();  // 8.750000
+                priceString = price.ToString("C"); // Money  8.75
+                priceString = price.ToString("N6:N2"); // 8.7500
+                priceString = String.Format("{0:C}", price);
+
+                //String interpolation - way to go
+                name = $"Hello {firstName} {lastName}, the year is {year:0000}.";
+
+            }
+        }
+    }
+}
---
+++ classwork/MovieLibrary/MovieLibrary.ConsoleHost/Program.cs
@@ -0,0 +1,205 @@
+﻿// ITSE 1430
+// Movie Library
+using System;
+
+namespace MovieLibrary.ConsoleHost
+{
+    //Main program
+    class Program
+    {
+        //Entry point function
+        static void Main(string[] args)
+        {
+            bool done = false;
+
+            do
+            {
+                char choice = GetInput();
+                switch (choice)
+                {
+                    case 'Q': done = HandleQuit(); break;
+                    case 'A': AddMovie(); break;
+                    case 'V': ViewMovie(); break;
+                    case 'D': DeleteMovie(); break;
+                    default: DisplayError("Unknown Option"); break;
+                };
+            } while (!done);
+        }
+
+        static Movie movie = new Movie();
+
+        private static void DeleteMovie ()
+        {
+            //Confirm
+            if (!ReadBoolean("Are you sure (Y/N)? "))
+                return;
+
+            //delete movie
+            movie.title = null;
+        }
+
+        static void AddMovie()
+        {
+            //Movie details - ignoring warnings for now...
+
+             movie.title = ReadString("Enter the movie title: ", true);                     // Required
+             movie.description = ReadString("Enter the optional description: ", false);     // Optional
+
+             movie.runLength = ReadInt32("Enter run length (in minutes): ", 0);                // >= 0
+             movie.releaseYear = ReadInt32("Enter the release year (min 1900): ", 1900);       // 1900+
+
+            //double reviewRating;     // Optional, 0.0 to 5.0
+             movie.rating = ReadString("Enter the MPAA rating: ", false);           // Optional, MPAA (not enforced)
+             movie.isClassic = ReadBoolean("Is this a classic (Y/N)? ");          // Optional
+        }
+
+        static void ViewMovie ()
+        {
+            //What if they haven't added one yet?
+            if (String.IsNullOrEmpty(movie.title))
+            {
+                Console.WriteLine("No movie available");
+                return;
+            }
+
+
+
+            Console.WriteLine($"{movie.title} ({movie.releaseYear})");
+            Console.WriteLine($"Runtime: {movie.runLength} mins");
+            Console.WriteLine($"MPAA Rating {movie.rating}");
+            Console.WriteLine($"Classic? {movie.isClassic}");
+            Console.WriteLine(movie.description);
+        }
+
+        static int ReadInt32 ( string message, int minimimValue )
+        {
+            Console.Write(message);
+
+            //Validate input
+            do
+            {
+                //string input = Console.ReadLine();
+                var input = Console.ReadLine();
+
+
+                //TODO: Input validation
+                //int result = Int32.Parse(input); // Crashes program, not good for input
+                //int result;
+
+                //if string parsed AND result is at least minimum value
+                //if (Int32.TryParse(input, out result))
+                //    if (result >= minimimValue)
+                if (Int32.TryParse(input, out var result) && result >= minimimValue)
+                    return result;
+
+                DisplayError("The value must be an integral value >= " + minimimValue);
+            } while (true);
+
+            //return -1;  not needed
+        }
+
+        static string ReadString ( string message, bool required )
+        {
+            Console.Write(message);
+
+            do
+            {
+                string input = Console.ReadLine().Trim();
+
+                // Is required
+                if (!String.IsNullOrEmpty(input) || !required)
+                    return input;
+
+                DisplayError("Value is required");
+            } while (true); 
+        }
+
+        static void DisplayError( string messsage )
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(messsage);
+            Console.ResetColor();
+        }
+
+        private static bool HandleQuit ()
+        {
+           // return ReadBoolean("Are you sure you want to quit (Y/N)? ");
+
+            //Display a confirmation
+            if (ReadBoolean("Are you sure you want to quit (Y/N)? "))
+                return true;
+
+            //Return results
+            return true;
+        }
+
+        private static bool ReadBoolean ( string message )
+        {
+            Console.Write(message);
+
+            do
+            {
+                ConsoleKeyInfo input = Console.ReadKey(true);
+                if (input.Key == ConsoleKey.Y)
+                    return true;
+                else if (input.Key == ConsoleKey.N)
+                    return false;
+            } while (true);
+            
+            //Not needed anymore
+            //return false;
+        }
+
+        static char GetInput()
+        {
+            Console.WriteLine("Movie Library");
+            //Console.WriteLine("---------------");
+            Console.WriteLine("".PadLeft(15, '-'));
+
+            Console.WriteLine("A) dd");
+            Console.WriteLine("V) iew");
+            Console.WriteLine("D) elete");
+            Console.WriteLine("Q) uit");
+
+            while (true)
+            {
+                //Get input
+                string input = Console.ReadLine().Trim();
+                //if (input == "Q")
+                //    return 'Q';
+                //else if (input == "A")
+                //    return 'A';
+                //else if (input == "V")
+                //    return 'V';
+                //else if (input == "D")
+                //    return 'D';
+
+                // Case insensitive
+                switch (input.ToUpper())
+                {
+                    //No fall thru, unless case statement empty
+                    //Must end with return or break;
+                    //case "B": input = "10"; break;
+                    //case "c":
+                    case "C": return 'B';
+
+                    //case "q":
+                    case "Q": return 'Q';
+
+                    //case "a":
+                    case "A": return 'A';
+
+                    //case "v":
+                    case "V": return 'V';
+
+                    //case "d":
+                    case "D": return 'D';
+                    //default:;
+                };
+
+                DisplayError("Invalid input");
+            };
+            //return default(char); //default
+        }
+    }
+}
