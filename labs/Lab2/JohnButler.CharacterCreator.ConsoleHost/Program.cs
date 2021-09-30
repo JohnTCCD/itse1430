65754ddc99dc96f0baecdec25295c396bddca089
John Butler <john.butler984@my.tccd.edu>
2021-09-30 00:49:07 -0500

    Initial version

---
+++ labs/Lab2/JohnButler.CharacterCreator.ConsoleHost/Program.cs
@@ -0,0 +1,20 @@
+﻿/*========================
+John Butler
+ITSE 1430 Fall 2021
+Lab 2 : Character Creator
+========================*/
+
+using System;
+
+namespace JohnButler.CharacterCreator.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ITSE 1430 Character Creator");
+            Console.WriteLine("John Butler  Fall 2021");
+
+        }
+    }
+}
