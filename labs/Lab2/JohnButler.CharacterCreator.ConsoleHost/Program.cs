fc37f28dc7b679c0c3db1a6c9232602c30e983f4
John Butler <john.butler984@my.tccd.edu>
2021-09-30 01:33:31 -0500

    DisplayMainMenu() added. GetMenuChoice() added. DisplayError() added.

--- labs/Lab2/JohnButler.CharacterCreator.ConsoleHost/Program.cs
+++ labs/Lab2/JohnButler.CharacterCreator.ConsoleHost/Program.cs
@@ -15,6 +15,54 @@ namespace JohnButler.CharacterCreator.ConsoleHost
             Console.WriteLine("ITSE 1430 Character Creator");
             Console.WriteLine("John Butler  Fall 2021");
 
+            do
+            {
+                DisplayMainMenu();
+                int choice = GetMenuChoice();
+            } while (true);
+
+        }
+
+        static void DisplayMainMenu()
+        {
+            Console.WriteLine("[1] Add Character");
+            Console.WriteLine("[2] View Character");
+            Console.WriteLine("[3] Edit Character");
+            Console.WriteLine("[4] Delete Character");
+            Console.WriteLine("[5] Quit");
+        }
+
+        static int GetMenuChoice ()
+        {
+            do
+            {
+                Console.WriteLine("What do you want to do?");
+                string input = Console.ReadLine();
+
+                if (String.IsNullOrEmpty(input))
+                {
+                    DisplayError("Input is required.");
+                    continue;
+                }
+
+                if (Int32.TryParse(input, out int value))
+                {
+                    if (value >= 1 && value <= 5)
+                        return value;
+                    else
+                        DisplayError("Value of input is not in range. (1 - 5)");
+                } 
+                else
+                    DisplayError("Input must be an integer.");
+
+            } while (true);
+        }
+
+        static void DisplayError ( string errorMessage )
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(errorMessage);
+            Console.ResetColor();
         }
     }
 }
