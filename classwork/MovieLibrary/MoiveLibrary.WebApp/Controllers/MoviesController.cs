7d5b66f7c7afea8a25009214379dfac6104193ac
John Butler <john.butler984@my.tccd.edu>
2021-12-08 21:25:05 -0600

    Create(), Delete(), Edit() added

--- classwork/MovieLibrary/MoiveLibrary.WebApp/Controllers/MoviesController.cs
+++ classwork/MovieLibrary/MoiveLibrary.WebApp/Controllers/MoviesController.cs
@@ -23,6 +23,92 @@ namespace MoiveLibrary.WebApp.Controllers
             return View(movies);
         }
 
+        [HttpGet]
+        public IActionResult Edit ( int id )
+        {
+            var movie = _database.Get(id);
+            if (movie == null)
+                return NotFound();
+
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Edit ( int id, Movie movie )
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _database.Update(movie.Id, movie);
+
+                    //PRG - post, redirect (to list), get
+                    return RedirectToAction(nameof(List));
+                }
+            } catch (Exception e)
+            {
+                ModelState.AddModelError("", e.Message);
+            }
+
+            return View(movie);
+        }
+
+        [HttpGet]
+        public IActionResult Create ()
+        {
+            var movie = new Movie();
+
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Create (int id, Movie movie)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _database.Add(movie);
+
+                    //PRG - post, redirect (to list), get
+                    return RedirectToAction(nameof(List));
+                }
+            } catch (Exception e)
+            {
+                ModelState.AddModelError("", e.Message);
+            }
+
+            return View(movie);
+        }
+
+        [HttpGet]
+        public IActionResult Delete ( int id )
+        {
+            var movie = _database.Get(id);
+            if (movie == null)
+                return NotFound();
+
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Delete ( int id, Movie movie )
+        {
+            try
+            {
+                _database.Delete(id);
+
+                //PRG - post, redirect (to list), get
+                return RedirectToAction(nameof(List));
+                
+            } catch (Exception e)
+            {
+                ModelState.AddModelError("", e.Message);
+            }
+
+            return View(movie);
+        }
+
         private readonly IMovieDatabase _database;
     }
 }
